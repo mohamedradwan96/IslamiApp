4b20b1ac33709f478d1f23fbfadabb91b36cdec1
mohamedradwan96 <eng.mohamedradwan996@gmail.com>
2022-11-22 23:17:20 +0200

    delete comment

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = "HomeScreen";
-  // dev
   @override
   Widget build(BuildContext context) {
     return Scaffold(
