06e4a8f2aed4541fbdd436a3d1c124fdf5b7f5fd
mohamedradwan96 <eng.mohamedradwan996@gmail.com>
2022-11-28 13:05:10 +0200

    edit hadith theme

--- lib/hadeth_details.dart
+++ lib/hadeth_details.dart
@@ -42,7 +42,7 @@ class HadethDetails extends StatelessWidget {
               itemBuilder: (_, index) {
                 return Text(
                   args.content[index],
-                  style: Theme.of(context).textTheme.subtitle1,
+                  style: Theme.of(context).textTheme.bodyText1,
                   textAlign: TextAlign.center,
                 );
               }),
