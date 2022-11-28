6719ce5efaca1a701047590ef540da74f847edee
mohamedradwan96 <eng.mohamedradwan996@gmail.com>
2022-11-28 14:38:54 +0200

    add sebha images

--- lib/home/tabs/sebha.dart
+++ lib/home/tabs/sebha.dart
@@ -5,7 +5,26 @@ class SebhaScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,);
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/images/background.png",
+          width: double.infinity,
+          fit: BoxFit.fitWidth,
+        ),
+        Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/sebhabody.png",
+                  width: 180,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
